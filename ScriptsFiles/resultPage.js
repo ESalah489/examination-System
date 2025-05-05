40994c1146b0a842c2e3764cc4bf9e8c3656621d
ebrahim salah <ebrahimsalah489@gmail.com>
2025-05-05 23:33:10 +0300

    chore(result.js,display answers of questions for student)

--- ScriptsFiles/resultPage.js
+++ ScriptsFiles/resultPage.js
@@ -1,3 +1,17 @@
+/* ---------------------------------- Pobup --------------------------------- */
+let allQuestions = document.querySelector("._allQuestions");
+
+let Target = document.querySelector("#Target");
+let pobup = document.getElementById("pobup");
+let card = document.getElementById("card");
+Target.addEventListener("click", function () {
+  pobup.style.cssText = "display:flex;transition: all 0.4s linear;";
+});
+pobup.addEventListener("click", function (e) {
+  if (!card.contains(e.target)) {
+    pobup.style.display = "none";
+  }
+});
 /* ---------------------------- if time is endded --------------------------- */
 let result = document.querySelector("#_result");
 let ifStudentFailed = document.querySelector("._uniqe");
@@ -24,6 +38,9 @@ if (localStorage.getItem("endedDate") !== null) {
         for (let i = 0; i < res.length; i++) {
           if (res[i] === correctAnswer[i]) {
             finalDegree += 10;
+            allQuestions.innerHTML += `<p style='color:green;'>${res[i]}</p>`;
+          } else {
+            allQuestions.innerHTML += `<p style='color:red;'>${res[i]}</p>`;
           }
         }
         if (finalDegree <= (res.length * 10) / 2) {
